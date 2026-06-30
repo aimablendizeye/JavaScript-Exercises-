f3159a75d1bf168cbaedaf6de5dbb8aba7ff9c02
Aimable Ndizeye <naimable883@gmail.com>
2026-06-30 09:44:41 +0200

    Q8 is added

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ console.log (sumUnique([1, 2, 2, 3, 4, 4, 5]));
 
 
 
-// Q2   
+// Q3 
  
 // **Mirror Image Words**
 
@@ -78,7 +78,7 @@ console.log(isMirrorImage("Test", "tseT "));
 
 
 
-// Q 3 
+// Q 4 
 
 // Write a function that takes in an array of digits and return the highest and lowest digits from it. Solve it without using `Math.max()` and `Math.min()`
 
@@ -116,7 +116,7 @@ console.log(highAndLow([1, 2, 3, 4, 5]));
 
 
 
-//Q  4
+//Q 5
 
 //Function that adds only even numbers 
 
@@ -140,7 +140,7 @@ console.log(sum([4,5,3,6,3,5]));
 
 
 
-// Q5
+// Q6
 
 
 const notifications= [
@@ -163,7 +163,7 @@ for (let messages of notifications) {
 
 
 
-// Q6
+// Q7
 
 
 function fullarr (arr1,arr2) {
@@ -176,10 +176,47 @@ function fullarr (arr1,arr2) {
 }
 
 
-
-
-
 console.log(fullarr([1,2,3],[4,5,6]));
+
+
+
+// Q 8 
+
+// You have to create a function which receives 3 number arguments: 2 operands a and b, and the result of an unknown operation performed on them.
+// Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+// The possible return strings are: "addition", "subtraction", "multiplication", "division".
+// Examples:
+// (a = 1, b = 2, result = 3)   --> 1 ? 2 = 3   --> "addition"
+// (a = 5, b = 2, result = 2.5) --> 5 ? 2 = 2.5 --> "division"
+// Notes
+// The / operator performs a plain division without rounding.
+// You can assume that there will always be a unique valid answer (no ambiguous cases like e.g. 1 ? 0 = 0 which could be either - or +, or 3 ? 1 = 3 which could be either * or /).
+// You can assume that there will be no division by 0
+
+
+
+function calcType(a, b, res) {
+  if (res === a + b){
+    return "addition"
+    
+  }
+  else if (res === a - b){
+    return "subtraction"
+  }
+  else if (res === a / b){
+    return "division"
+  }
+  
+  else if (res === a * b){
+    return "multiplication"
+  }
+  else if (b === 0 ){
+    return `you can't divide by ${this.b}`
+  }
+  
+}
+
+console.log(calcType(3,5,-2));
 
 
 
