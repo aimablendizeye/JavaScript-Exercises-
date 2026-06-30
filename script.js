a144f690430f2cdb07139cbb2a73f3e9ad9dfa92
Aimable Ndizeye <naimable883@gmail.com>
2026-06-30 09:47:19 +0200

    Q9 is Added

--- script.js
+++ script.js
@@ -220,6 +220,30 @@ console.log(calcType(3,5,-2));
 
 
 
+// Q 9 
+
+// Suzuki needs help lining up his students!
+
+// Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+
+// You will be given a string of student names. Sort them and return a list of names in descending order.
+
+function lineupStudents(students){
+  
+  return students.split(" ").sort((a,b) => {
+      if (b.length !== a.length){
+        return b.length - a.length;
+      }
+      return b.localeCompare(a)
+    })
+   
+   
+  }
+
+console.log(lineupStudents ( 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'))
+
+
+
 
 
 
