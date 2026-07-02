7fa77e8da5ca582da31e304cd8ffeee6324341e2
Aimable Ndizeye <naimable883@gmail.com>
2026-07-02 10:13:18 +0200

    Q12 was added

--- script.js
+++ script.js
@@ -287,6 +287,7 @@ console.log(counti("wwde"));
 
 
 // Q12 
+
 // Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
 
 // Example 1:
@@ -313,6 +314,30 @@ function twoArray (arr1,arr2){
 console.log(twoArray(["arp", "live", "strong"],["lively", "alive", "harp", "sharp", "armstrong"]));
 
 
+// Q 13
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If a number is a multiple of both 3 and 5, only count it once.
+
+
+function solution(number){
+   if (number < 0) return 0;
+  
+  let sum = 0;
+  for (let i = 3; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+  
+}
+console.log(solution (5));
 
 
 
